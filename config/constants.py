971908cd2eeedcc67d121a002bc0ad0677fd5f27
LeeJongEun <bellsilver7@jasongroup.co.kr>
2023-03-21 09:56:13 +0900

    create program model

---
+++ config/constants.py
@@ -0,0 +1,5 @@
+STREAM_STATUS_CHOICES = (
+    ('WAITING', '대기중'),
+    ('STREAMING', '방송중'),
+    ('ENDED', '종료'),
+)
--- live/models.py
+++ live/models.py
@@ -1,3 +1,17 @@
+import uuid
+
 from django.db import models
 
+from config.constants import STREAM_STATUS_CHOICES
+
+
 # Create your models here.
+class Program(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    thumbnail = models.ImageField(null=True, upload_to='programs/')
+    stream_status = models.CharField(max_length=20, choices=STREAM_STATUS_CHOICES)
+    stream_scheduled_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
