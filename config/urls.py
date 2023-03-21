cb125e398ca2961f793f6408f580fd06c86ab4c6
LeeJongEun <bellsilver7@jasongroup.co.kr>
2023-03-21 09:57:50 +0900

    programs routing

--- config/urls.py
+++ config/urls.py
@@ -14,30 +14,39 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+
+from live.models import Program
 
 
 # Serializers define the API representation.
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['url', 'username', 'email', 'is_staff']
+class ProgramSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Program
+        fields = [
+            'id',
+            'title',
+            'description',
+            'thumbnail',
+            'stream_status',
+            'stream_scheduled_at',
+            'created_at',
+            'updated_at',
+        ]
 
 
 # ViewSets define the view behavior.
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
+class ProgramViewSet(viewsets.ModelViewSet):
+    queryset = Program.objects.all()
+    serializer_class = ProgramSerializer
 
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-# router.register(r'users', UserViewSet)
+router.register(r'programs', ProgramViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
