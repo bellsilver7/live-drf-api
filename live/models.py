fe5577b53744d53d08bc18fcf22cc9d8a5e9d357
LeeJongEun <bellsilver7@jasongroup.co.kr>
2023-03-22 15:39:58 +0900

    모델 수정

--- live/models.py
+++ live/models.py
@@ -9,9 +9,9 @@ from config.constants import STREAM_STATUS_CHOICES
 class Program(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     title = models.CharField(max_length=30)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, null=True)
     thumbnail = models.ImageField(null=True, upload_to='programs/')
     stream_status = models.CharField(max_length=20, choices=STREAM_STATUS_CHOICES)
     stream_scheduled_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
