0ed848f786d620b40090c9aa24997be818d1682e
LeeJongEun <bellsilver7@jasongroup.co.kr>
2023-04-10 18:18:42 +0900

    django-seed 버전 다운그레이드, 마이그레이션 재생성

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Django REST Framework
+
+### Poetry
+
+```shell
+# 패키지 추가 (dev는 -D 옵션)
+$ poetry add {패키지}
+
+# 정의된 패키지 설치
+$ poetry install
+
+# 설치된 패키지 삭제
+$ poetry remove {패키지}
+
+# 패키지 동기화
+$ poetry install --sync
+$ poetry install --without dev --sync
+$ poetry install --with docs --sync
+$ poetry install --only dev
+```
+
+### Seed
+
+```shell
+# 프로그램
+$ python manage.py seed_programs --number={개수}
+
+# 호스트
+$ python manage.py seed_hosts --number={개수}
+```
--- lives/models.py
+++ lives/models.py
@@ -13,14 +13,13 @@ STREAM_STATUS_CHOICES = [
 
 # Create your models here.
 class Program(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=100, null=True)
     thumbnail = models.ImageField(upload_to='lives/program', null=True)
     stream_status = models.CharField(choices=STREAM_STATUS_CHOICES, default='upcoming', max_length=100)
     stream_scheduled_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Host(models.Model):
@@ -33,4 +32,4 @@ class Host(models.Model):
     short_description = models.TextField()
     profile_image = models.ImageField(null=True, upload_to='lives/host')
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
