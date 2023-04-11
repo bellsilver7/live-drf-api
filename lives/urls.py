e74ba74390c11f04b84453019a03be9c67d2d288
LeeJongEun <bellsilver7@jasongroup.co.kr>
2023-04-11 10:20:19 +0900

    media urlconf

--- lives/urls.py
+++ lives/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework import routers
 
+from config import settings
 from lives.views import ProgramViewSet
 
 router = routers.DefaultRouter()
@@ -9,3 +11,8 @@ router.register('programs', ProgramViewSet)
 urlpatterns = [
     path('', include(router.urls))
 ]
+
+urlpatterns += static(
+    prefix=settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
